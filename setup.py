9a74b4ac678402003c790da2a7ccb5a31d6d554a
Jingshu Wang <wangjingshususan@gmail.com>
2019-10-07 22:41:05 -0500

    Can not work with tensorflow 2.0.0 now. Will change the syntax when tensorflow 2 gets more popular

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
     setuptools.setup(
             name="sctransfer",
-            version="0.0.5",
+            version="0.0.6",
             author="Jingshu Wang",
             author_email="wangjingshususan@gmail.com",
             description="Python part for scRNA-seq transfer learning denoising tool SAVER-X",
@@ -16,7 +16,7 @@ with open("README.md", "r") as fh:
             python_requires='>=3.5',
             install_requires=['numpy>=1.7',
                 'keras>=2.2.2',
-                'tensorflow',
+                'tensorflow<=1.14.0',
                 'h5py',
                 'six>=1.10.0',
                 'scikit-learn',
